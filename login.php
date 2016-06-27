5dbe326457dbc244dee0a493e4556a13a30b1fef
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:49:16 +0530

    indentation

--- login.php
+++ login.php
@@ -24,19 +24,23 @@ font-family:sylfaen;
 <br>
 <form action="check.php" method="post" style="font-size:40px;font-family:sylfaen">
 <fieldset style="border:5px solid black;" >
-<legend style="font-size:50px;font-family:sylfaen;"><u><b> LOGIN </b></u></legend>
-<center>
-USERNAME
-<br>
-<input type="text" name="username" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
-<br>
-<br>
-PASSWORD
-<br>
-<input type="text" name="password" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
-<br>
-<br>
-<input type="submit" style="background-color:black; color:rgb(163,0,163);font-size:40px;font-family:sylfaen;" value="Submit">
+  <legend style="font-size:50px;font-family:sylfaen;"><u><b> LOGIN </b></u></legend>
+  <center>
+  <!--input username and password-->
+  
+  USERNAME
+  <br>
+  <input type="text" name="username" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
+  <br>
+  
+  <br>
+  PASSWORD
+  <br>
+  <input type="text" name="password" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
+  <br>
+  
+  <br>
+  <input type="submit" style="background-color:black; color:rgb(163,0,163);font-size:40px;font-family:sylfaen;" value="Submit">
 </fieldset>
 </form>
 </center>

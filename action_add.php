74596b96c4f0eb58cb472f6b14738d51500c57d7
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:24:41 +0530

    indentation

--- action_add.php
+++ action_add.php
@@ -5,29 +5,33 @@
 ADD RESULT
 </title>
 </head>
+
 <body style="background-color:rgb(255,51,102);">
+  
 <center>
 <p style=";font-size:40px;font-family:sylfaen">
-<?php
-// Create connection
-$h="localhost";$d="BulletinDB";$r="root";$p="";
-$conn = new mysqli("localhost", "root", "", "BulletinDB");
-$dbh=new PDO("mysql:host=$h;dbname=$d", $r, $p);
-$sql = $dbh->prepare("INSERT INTO User (Name,ID,Post,Email,UserName,Access,Password)
-VALUES (:a,:b,:c,:d,:e,:f,:g)");
-$sql->bindParam(':a',$_POST["name"]);
-$sql->bindParam(':b',$_POST["ID"]);
-$sql->bindParam(':c',$_POST["post"]);
-$sql->bindParam(':d',$_POST["mail"]);
-$sql->bindParam(':e',$_POST["username"]);
-$sql->bindParam(':f',$_POST["access"]);
-$sql->bindParam(':g',$_POST["password"]);
-$sql->execute();
-echo "You have registered successfully!";
-echo "Please login now";
-$conn->close();
-?>
+  <?php
+  // Create connection
+  $h="localhost";$d="BulletinDB";$r="root";$p="";
+  $conn = new mysqli("localhost", "root", "", "BulletinDB");
+  $dbh=new PDO("mysql:host=$h;dbname=$d", $r, $p);
+  //writing new record
+  $sql = $dbh->prepare("INSERT INTO User (Name,ID,Post,Email,UserName,Access,Password)
+  VALUES (:a,:b,:c,:d,:e,:f,:g)");
+  $sql->bindParam(':a',$_POST["name"]);
+  $sql->bindParam(':b',$_POST["ID"]);
+  $sql->bindParam(':c',$_POST["post"]);
+  $sql->bindParam(':d',$_POST["mail"]);
+  $sql->bindParam(':e',$_POST["username"]);
+  $sql->bindParam(':f',$_POST["access"]);
+  $sql->bindParam(':g',$_POST["password"]);
+  $sql->execute();
+  echo "You have registered successfully!";
+  echo "Please login now";
+  $conn->close();
+  ?>
 </p>
+//leading to login page
 <br>
 <form action="login.php" id="form1"></form>
 <button class="button" form="form1" style="background-color:black;font-size:35px;color:rgb(255,51,102);padding:20px 20px;font-family:sylfaen;"> LOGIN </button>

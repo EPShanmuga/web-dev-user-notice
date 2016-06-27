091bbc7ff5f348916796c1231f7374c10edff7af
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:10:59 +0530

    Required files.the main.htm is the root page or the home page which directs to add.htm and action_add for new user registration or login and page.php for existing users

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+LOGIN PAGE
+</title>
+</head>
+<style>
+h
+{
+background-color:black;
+color:rgb(0,153,255);
+font-family-algerian;
+font-size:50px;
+}
+p
+{
+font-size:25px;
+font-family:sylfaen;
+}
+</style>
+<body style="background-color:rgb(204,51,155);">
+<br>
+<br>
+<form action="check.php" method="post" style="font-size:40px;font-family:sylfaen">
+<fieldset style="border:5px solid black;" >
+<legend style="font-size:50px;font-family:sylfaen;"><u><b> LOGIN </b></u></legend>
+<center>
+USERNAME
+<br>
+<input type="text" name="username" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
+<br>
+<br>
+PASSWORD
+<br>
+<input type="text" name="password" style="background-color:black;color:rgb(163,0,163);font-size:25px;">
+<br>
+<br>
+<input type="submit" style="background-color:black; color:rgb(163,0,163);font-size:40px;font-family:sylfaen;" value="Submit">
+</fieldset>
+</form>
+</center>
+</body>
+<html>

7a9507e1ed46da1f6e93c8fdd30201700fb12485
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:36:58 +0530

    indentation

--- change_action.php
+++ change_action.php
@@ -4,27 +4,30 @@
 RESULT
 </title>
 </head>
+
 <body style="background-color:rgb(204,255,51);">
 <center>
 <p style=";font-size:40px;font-family:sylfaen">
-<?php
-$a= $_GET['user'];
-$b= $_GET['change'];
-$conn = new mysqli("localhost", "root", "", "BulletinDB");
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "UPDATE User SET Access='$b' WHERE UserName='$a'";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Access changed successfully!";
-} else {
-    echo "Error updating record: " . $conn->error;
-}
-
-$conn->close();
+    <?php
+    $a= $_GET['user'];
+    $b= $_GET['change'];
+    $conn = new mysqli("localhost", "root", "", "BulletinDB");
+    // Check connection
+    if ($conn->connect_error) 
+    {
+     die("Connection failed: " . $conn->connect_error);
+    }
+    //updating old news by new news
+    $sql = "UPDATE User SET Access='$b' WHERE UserName='$a'";
+    if ($conn->query($sql) === TRUE) 
+    {
+        echo "Access changed successfully!";
+    } 
+    else 
+    {
+        echo "Error updating record: " . $conn->error;
+    }
+    $conn->close();
 ?>
 </p>
 </body>

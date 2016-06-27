fab20af657826b19fa1a87d836dc02be915bb2bd
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:35:11 +0530

    indentation

--- change.php
+++ change.php
@@ -4,6 +4,7 @@
 RESULT
 </title>
 </head>
+
 <body style="background-color:rgb(255,51,102);font-size:25px;font-family:sylfaen;">
 <center>
 <?php
@@ -12,37 +13,41 @@ $sql = "SELECT Name,UserName,Access FROM User";
 $result = $conn->query($sql);
 $var=1;
 if ($result->num_rows > 0)
- 
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-echo "<br>".$var;
-
-echo "<b>"." Name: "."</b>" . $row["Name"]."<br>";
-echo "<b>"." Username: "."</b>".$row["UserName"]."<br>";
-if($row["Access"]!="admin")
-{$q=$row["Access"];
-$p=$row["UserName"];
-echo '<form action="change_action.php" method="get">
-<input type="hidden" value="'.$p.'" name="user">
-<b>Access:</b>
-<select name="change" style="background-color:rgb(255,51,102);font-size:25px;font-family:sylfaen;border:ipx solid black;">
-<option selected="selected">
-'.$q.'
-</option>
-<option value="admin">Admin</option>
-<option value="Editor">Editor</option>
-<option value="Viewer">Viewer</option>
-</select>
-<br>
-<input type="submit" value="change" style="background-color:rgb(255,51,102);font-size:25px;font-family:sylfaen;"></form>';}
-else
-echo "<b>"." Access: "."</b>".$row["Access"]."<br>";
-$var++;
-}
+ // output data of each row
+ while($row = $result->fetch_assoc()) 
+ {
+   echo "<br>".$var;
+   echo "<b>"." Name: "."</b>" . $row["Name"]."<br>";
+   echo "<b>"." Username: "."</b>".$row["UserName"]."<br>";
+   //gives an option of changing access of non-admin users with a drop-down box while printing the present level
+  if($row["Access"]!="admin")
+  {
+   $q=$row["Access"];
+   $p=$row["UserName"];
+   echo '<form action="change_action.php" method="get">
+   <input type="hidden" value="'.$p.'" name="user">
+   
+   <b>Access:</b>
+   <select name="change" style="background-color:rgb(255,51,102);font-size:25px;font-family:sylfaen;border:ipx solid black;">
+    <option selected="selected">
+    '.$q.'
+    </option>
+    <option value="admin">Admin</option>
+    <option value="Editor">Editor</option>
+    <option value="Viewer">Viewer</option>
+   </select>
+   <br>
+   
+   <input type="submit" value="change" style="background-color:rgb(255,51,102);font-size:25px;font-family:sylfaen;"></form>';
+  }
+  else
+  //unchangeablbe access of admins
+   echo "<b>"." Access: "."</b>".$row["Access"]."<br>";
+  $var++;
+ }
  else 
-    echo "No results";
-
-$conn->close();
+  echo "No results";
+ $conn->close();
 ?>
 </center>
 </body>

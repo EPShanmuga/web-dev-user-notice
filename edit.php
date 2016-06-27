6c32d8e53e87f39413a41fdf788620b6eb732a25
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:45:49 +0530

    indentation

--- edit.php
+++ edit.php
@@ -5,18 +5,22 @@
 EDIT PAGE
 </title>
 </head>
+
 <body style="background-color:rgb(255,102,51);font-size:30px;font-family:sylfaen;">
 <center>
 <h1 style="background-color:rgb(255,102,51);font-size:40px;font-family:sylfaen;">
 <p style=";font-size:35px;font-family:sylfaen">OLD NEWS</p>
-<?php
-$a= $_GET['user'];
-echo $a;
-echo '<p style=";font-size:35px;font-family:sylfaen">NEW NEWS</p>';
-echo '<form action="edit_action.php" method="get">
-<input type="hidden" name="old" value="'.$a.'" >
-<input type="text" style="background-color:rgb(255,102,51);font-size:30px;font-family:sylfaen;" name="new" value="'.$a.'" ><br><br>
-<input type="submit" style="background-color:black;color:rgb(255,102,51);font-size:30px;font-family:sylfaen;padding:10px 10px;" value="edit"></form>';
-?>
+  <?php
+    $a= $_GET['user'];
+    //print the old news
+    echo $a;
+    echo '<p style=";font-size:35px;font-family:sylfaen">NEW NEWS</p>';
+     //input the alteration
+    echo '<form action="edit_action.php" method="get">
+    <input type="hidden" name="old" value="'.$a.'" >
+    <input type="text" style="background-color:rgb(255,102,51);font-size:30px;font-family:sylfaen;" name="new" value="'.$a.'" ><br><br>
+    <input type="submit" style="background-color:black;color:rgb(255,102,51);font-size:30px;font-family:sylfaen;padding:10px 10px;" value="edit"></form>';
+  ?>
+</p>
 </body>
 </html>

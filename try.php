1e7de716632570df2e1dc97e36a4790a826991a0
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:51:52 +0530

    indentation

--- try.php
+++ try.php
@@ -4,12 +4,23 @@
 <title>
 ADD NEWS
 </title>
-</head><center>
+</head>
+<center>
+  
 <body style="background-color:rgb(204,255,51);font-family:sylfaen;">
-<h1 style="font-size:40px;"><u>THE NEWS PIECE TO BE ADDED:</u></h1>
+  <!--inputs the new news item-->
+<h1 style="font-size:40px;">
+  <u>
+    THE NEWS PIECE TO BE ADDED:
+  </u>
+</h1>
 <form action="insertnoticetable.php" method="get">
-<input type="text" name="news" style="background-color:rgb(204,255,51);font-family:sylfaen;font-size:30px;">
-<br><br><input style="background-color:black; color:rgb(204,255,51);font-size:30px;" type="submit" value="ADD"></form>';
+  <input type="text" name="news" style="background-color:rgb(204,255,51);font-family:sylfaen;font-size:30px;">
+  <br>
+  <br>
+  <input style="background-color:black; color:rgb(204,255,51);font-size:30px;" type="submit" value="ADD">
+</form>';
 </body>
+
 </center>
 </html>

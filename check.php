cbce2f2a563e0fbcba79fe8e7c2f1a776575dafc
EPShanmuga <shanmugaep8@gmail.com>
2016-06-28 00:43:21 +0530

    indentation

--- check.php
+++ check.php
@@ -5,46 +5,54 @@
 USER PAGE
 </title>
 </head>
+
 <body style="background-color:rgb(177,255,61); font-size:30px;font-family:sylfaen;">
 <?php
-// Create connection
-$conn = new mysqli("localhost", "root", "", "BulletinDB");
-// Check connection
-if ($conn->connect_error)
-{
-die("Connection failed: " . $conn->connect_error);
-} 
-$flag=0;
-$sql = "SELECT Name,ID,Post,Email,UserName,Access,Password FROM User";
-$result = $conn->query($sql);
-if ($result->num_rows > 0)
-{
-// output data of each row
-while($row = $result->fetch_assoc()) 
-{
-if($_POST["username"]==$row["UserName"])
-{
-if($_POST["password"]==$row["Password"]){//echo "yes password";
-$flag=1;
-break;}}
-}
-}
-$conn->close();
-$c=$_POST["username"];
-if(!$flag)echo("WRONG USERNAME OR PASSWORD");
-else
-{
-echo '<center><h1>PROCEED TO...?</h!?</center>';
-if($row['Access']=='Viewer')
-echo "<form action='vboard.php' method='post'><input type='submit' value='NOTICE BOARD' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;'></form>";
-if($row['Access']=='admin'||$row['Access']=='Admin')
-{echo "<form action='aboard.php' method='post'><input type='submit' value='NOTICE BOARD' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;'></form>";
-echo "<form action='change.php' method='post'><input type='submit' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;' value='CHANGE ACCESS'></form>";
-}
-if($row['Access']=='Editor')
-echo "<form action='eboard.php' method='post'><input type='submit' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;' value='NOTICE BOARD'></form>";}
-
-?>
-
+  // Create connection
+  $conn = new mysqli("localhost", "root", "", "BulletinDB");
+  // Check connection
+  if ($conn->connect_error)
+  {
+    die("Connection failed: " . $conn->connect_error);
+  } 
+  $flag=0;
+  //download data from database
+  $sql = "SELECT Name,ID,Post,Email,UserName,Access,Password FROM User";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0)
+  {
+  // checks for corresponding username and password
+    while($row = $result->fetch_assoc()) 
+    {
+      if($_POST["username"]==$row["UserName"])
+      {
+        if($_POST["password"]==$row["Password"])
+        { 
+          //in case of correct password and username
+          $flag=1;
+          break;
+        }  
+      }
+    }
+  }
+  $conn->close();
+  $c=$_POST["username"];
+  if(!$flag)
+    echo("WRONG USERNAME OR PASSWORD");
+  else
+  {
+    //correct password and username gives option to visit notice board(or to change access levels in case of an admin
+    echo '<center><h1>PROCEED TO...?</h!?</center>';
+    if($row['Access']=='Viewer')
+      echo "<form action='vboard.php' method='post'><input type='submit' value='NOTICE BOARD' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;'></form>";
+    if($row['Access']=='admin'||$row['Access']=='Admin')
+    {
+      echo "<form action='aboard.php' method='post'><input type='submit' value='NOTICE BOARD' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;'></form>";
+      echo "<form action='change.php' method='post'><input type='submit' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;' value='CHANGE ACCESS'></form>";
+    }
+    if($row['Access']=='Editor')
+      echo "<form action='eboard.php' method='post'><input type='submit' style='background-color:black;color:rgb(177,255,61);font-family:sylfaen;font-size:25px;padding:15px 15px;' value='NOTICE BOARD'></form>";
+    }
+  ?>
 </body>
 </html>
